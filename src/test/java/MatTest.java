67b152f71dea86967c83c733c422fbb86b365691
Your Name <fdfdfss@fedsf.com>
2023-11-24 08:51:11 +0200

    first commit

---
+++ src/test/java/MatTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MatTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void avgArray() {
+    }
+        @Test
+        void isMatrixSquare () {
+            Mat[][] mat = new Mat[5][5];
+            assertEquals(Mat[5],[5])
+    }
+}
